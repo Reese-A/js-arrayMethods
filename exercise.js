412d091347dfecc1c132f5bceb8280f19ecc6d47
Reese Akamine <Arresayy@gmail.com>
2018-02-07 19:41:39 -1000

    complete exercise 3

--- exercise.js
+++ exercise.js
@@ -34,7 +34,8 @@ console.log(shoppingList);
 /*3. Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
 var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
-
+yoda.reverse();
+console.log(yoda);
 
 
 /*The array.shift() removes the first element from an array and returns that element*/
